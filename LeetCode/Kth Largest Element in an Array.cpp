f430c4efbb9323330f48ecd4e07a30836428e69e
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2025-06-28 09:30:58 +0530

    Create Kth Largest Element in an Array.cpp

---
+++ LeetCode/Kth Largest Element in an Array.cpp
@@ -0,0 +1,6 @@
+// Approach 3/3: QuickSelect Algorithm
+// Inspired by the QuickSort algorithm, QuickSelect is a divide-and-conquer technique. 
+// It partitions the array around a pivot and recursively searches for the k-th largest element in the appropriate partition. 
+// When the pivot is chosen randomly, the algorithm tends to have a linear average-case time complexity, making it faster than the sorting approach for large datasets.
+
+// TODO : Learn more about Quick Select Technique and Divide and Conquer so that it can be a tool for other problems.
