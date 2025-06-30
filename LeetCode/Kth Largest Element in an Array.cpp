ae98ec052a86b173ca3d14b8f2ee72fedbf259cb
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2025-06-30 23:48:49 +0530

    Update Kth Largest Element in an Array.cpp

--- LeetCode/Kth Largest Element in an Array.cpp
+++ LeetCode/Kth Largest Element in an Array.cpp
@@ -3,4 +3,6 @@
 // It partitions the array around a pivot and recursively searches for the k-th largest element in the appropriate partition. 
 // When the pivot is chosen randomly, the algorithm tends to have a linear average-case time complexity, making it faster than the sorting approach for large datasets.
 
+// pivot_index = random.randint(left, right)
+
 // TODO : Learn more about Quick Select Technique and Divide and Conquer so that it can be a tool for other problems.
